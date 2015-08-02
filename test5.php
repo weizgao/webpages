8966647c6672a85a5019381603b45d0ea6b12906
wgao4000 <wgao4000@gmail.com>
2015-08-02 16:44:38 -0400

    Adding my existing site.

---
+++ test5.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+	<head>
+		<title>
+			My Website
+		</title>
+		<link rel="stylesheet"	type="text/css" href="style.css">
+	</head>
+	<body>
+		<header class="masthead">
+			<p class="masthead-intro">Hi, I'm</p>
+			<h1 class="masthead-heading">Wei!</h1>
+		</header>
+		<section class="introduction-section">
+			<h1>Introduction</h1>
+			<p>_INSERT_YOUR_3_SENTENCE_DESCRIPTON_HERE_</p>
+			<p>_INSERT _WHAT_IS_IMPORTANT_TO_YOU_HERE</p>
+		</section>
+		<section class="location-section">
+				<h1>Where I'm From</h1>
+				<p>_INSERT_YOUR_LOCATION_PARAGRAPH_HERE_</p>
+
+		</section>
+		<section class="questions-section">
+			<h1>More About me</h1>
+			<h2>What are your favorite hobbies?</h2>
+			<p>INSERT_YOUR_ANSWER_HERE</p>
+			<h2>What's your dream job?</h2>
+		</section>
+		
+		<footer class="content-footer">
+			<p>Say hi to me on these social networks:</p>
+			<ul class="social">
+				<li><a href="_INSERT_GitHUB_URL_HERE_">GitHub</a>
+				</li>
+				<li><a href="_INSERT_TIWTTER_URL_HERE_">Twitter</a></li>
+				</li>
+				<li><a href="_INSERT_GOOGLE+URL_HERE_">Google+</a></li>
+			</ul>
+		</footer>
+	</body>
+</html>
